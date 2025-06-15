74d18a97def19f11d42890cd6dbc010f36ea5b61
kavdupasd-09 <popmusicisoport@gmail.com>
2025-06-15 11:23:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'TM0zDCxZ#Ti53gxYhoUuds4YKSeTnYX9-1xeL8q9Bkf_z9XuM7aQ' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'eRNx1LaK#u0yrswik-GbSMPtQbUgiu8N6B9Bghg3ma6hd_wLvpGI' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
